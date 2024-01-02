157720db083b9fbc4e293b3d6ad52ec020550cb1
Dinazns <dinaz2000@hotmail.fr>
2024-01-02 17:11:44 +0100

    Modif code affichage dynamique et lightbox

--- scripts/utils/lightBox.js
+++ scripts/utils/lightBox.js
@@ -1,9 +1,9 @@
-const side_left = document.querySelector('.side_left');
-const side_right = document.querySelector('.side_right');
+// const side_left = document.querySelector('.side_left');
+// const side_right = document.querySelector('.side_right');
 
 // Fermer la modale
-document.querySelector('.lightBox span').addEventListener("click", () => {
-    document.querySelector('.lightBox').style.display = 'none';
+document.querySelector('#lightBox span').addEventListener("click", () => {
+    document.querySelector('#lightBox').style.display = 'none';
 });
 
 // // Evénements pour les flèches gauche et droite
